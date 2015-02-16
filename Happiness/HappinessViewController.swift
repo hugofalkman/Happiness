7f51dad97317ed997b763c359a7ca4b8980ae065
hugofalkman <zilla@Hs-MacBook-Pro.local>
2015-02-16 14:16:19 +0100

    Lecture 5 complete.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by H Hugo Falkman on 2015-02-16.
+//  Copyright (c) 2015 H Hugo Fakman. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
